9eb64549ebe19b9fbd27e269052d2c335cff53f7
sabaamajdi0-jpg <sabaamajdi0@gmail.com>
2026-01-05 14:17:33 +0300

    5/1/2026s

---
+++ task1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = Console.ReadLine();
+            Console.WriteLine(text);
+            double x = 11.1;
+            Console.WriteLine(x);
+            string name = "sabaa";
+            Console.WriteLine(name);
+            char myChar = 'o';
+            Console.WriteLine(myChar);
+            bool b = false;
+            Console.WriteLine(b);
+            int i = 8;
+            Console.WriteLine(i);
+
+            const int y = 1000;
+            Console.WriteLine(y);
+
+
+
+            string[] cars = { "Lexus", "Porsche", "Mercedes", "BMW" };
+
+            for (int s = 0; s < cars.Length; s++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+
+            Console.WriteLine("Array Length: " + cars.Length);
+
+            Console.WriteLine("input your first name: ");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("input your last name: ");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine("input your   year of  birth: ");
+            int year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(firstname + " " + lastname + " " + year);
+
+
+
+            int[] arr = new int[10];
+
+            for (int z = 0; z < arr.Length; z++)
+            {
+                Console.Write("element - " + z + " : ");
+                arr[z] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Elements in array are: ");
+            for (int z = 0; z < arr.Length; z++)
+            {
+                Console.Write(arr[z] + " ");
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
---
+++ task2/task2/task2/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            int evenCount = 0;
+            int oddCount = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Enter a number: ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num < 0)
+                {
+                    continue;
+                }
+
+                if (num == 0)
+                {
+                    break;
+                }
+
+                if (num % 2 == 0)
+                {
+                    evenCount++;
+                }
+                else
+                {
+                    oddCount++;
+                }
+            }
+
+            Console.WriteLine("Even numbers: " + evenCount);
+            Console.WriteLine("Odd numbers: " + oddCount);
+
+
+
+
+            int grade;
+
+            while (true)
+            {
+                Console.Write("Enter student grade (0-100): ");
+                grade = int.Parse(Console.ReadLine());
+
+                if (grade >= 0 && grade <= 100)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Invalid grade, try again.");
+            }
+
+            if (grade >= 90)
+            {
+                Console.WriteLine("Grade: A");
+            }
+            else if (grade >= 80)
+            {
+                Console.WriteLine("Grade: B");
+            }
+            else if (grade >= 70)
+            {
+                Console.WriteLine("Grade: C");
+            }
+            else if (grade >= 60)
+            {
+                Console.WriteLine("Grade: D");
+            }
+            else
+            {
+                Console.WriteLine("Grade: F");
+
+
+
+            }
+
+
+
+
+
+            }
+        }
+
+    } 
+  
